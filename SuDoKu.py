47b3f93634992bea3dd79b55cb25a908605a2bb9
helq2612 <heli@oregonstate.edu>
2018-02-18 00:12:26 -0800

    merged files

--- SuDoKu.py
+++ SuDoKu.py
@@ -19,20 +19,7 @@ def sudoku(initialState):
     print(initialState)
     # get data structures
     cells_status = get_initialDict(initialState)
-<<<<<<< HEAD
-=======
-    # forward checking
-    forward_checking(cells_status)
-    #print_sudoku(cells_status)
 
-    inference(cells_status, 11) # naked_single
-    inference(cells_status, 12) # hidden_single
-    inference(cells_status, 21) # naked_pairs
-    inference(cells_status, 22) # hidden_pairs
-    inference(cells_status, 31) # naked_triples
-    inference(cells_status, 32) # hidden_triples
-    #print_sudoku(cells_status)
->>>>>>> 221e53f136dd63e6e71d33c591b41c0e34589528
 
     # apply fixed baseline approach
     success, result_cells, used_steps = fixed_baseline(cells_status, 0, 0)
@@ -46,9 +33,6 @@ if __name__ == '__main__':
     filePath = 'data/sudoku-problems.txt'
     samples = getData(filePath)
 
-<<<<<<< HEAD
     test = samples[13][0]
-=======
-    test = samples[77][0]
->>>>>>> 221e53f136dd63e6e71d33c591b41c0e34589528
+
     sudoku(test)
